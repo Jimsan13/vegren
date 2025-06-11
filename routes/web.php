a1392b854d82f292920f40009405078d07f71a82
Jimsan13 <sandrajimenez150918@gmail.com>
2025-06-10 21:51:48 -0600

    Version del login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+ use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ContadorController;
+use App\Http\Controllers\CampoController;
+use App\Http\Controllers\AlmacenController;
+use App\Http\Controllers\NominaController;
+use Illuminate\Support\Facades\Auth;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +22,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->name('dashboard');
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::middleware(['auth', 'role:admin'])->get('/admin/dashboard', [AdminController::class, 'index']);
+Route::middleware(['auth', 'role:contador'])->get('/finanzas', [ContadorController::class, 'index']);
+Route::middleware(['auth', 'role:campo'])->get('/campo', [CampoController::class, 'index']);
+Route::middleware(['auth', 'role:almacen'])->get('/almacen', [AlmacenController::class, 'index']);
+Route::middleware(['auth', 'role:nomina'])->get('/nomina', [NominaController::class, 'index']);
+
+
