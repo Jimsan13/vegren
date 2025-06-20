d9554ea5b65c77a0b67120bec30b49f6c0736010
Jimsan13 <sandrajimenez150918@gmail.com>
2025-06-20 09:18:34 -0600

    Implementacion de codigo

--- resources/views/rol/admin/campo.blade.php
+++ resources/views/rol/admin/campo.blade.php
@@ -1,1 +1,35 @@
-<h1>hola soy campo </h1>
+@extends('layouts.app')
+
+@section('title', 'Dashboard de Administración') {{-- Esto establecerá el título en el <head> --}}
+
+@section('sidebar')
+    <x-admin.sidebar />
+@endsection
+
+@section('navbar')
+    <x-admin.navbar />
+@endsection
+
+@section('content')
+    <div class="d-flex justify-content-between align-items-center mb-4 content-header">
+        <h3>Gastos y Movimientos </h3>
+    </div>
+    <div class="row g-4">
+    </div>
+@endsection
+
+@section('scripts')
+    {{-- Script para el toggle del sidebar --}}
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            var sidebarToggle = document.getElementById('sidebarToggle');
+            var wrapper = document.getElementById('wrapper');
+
+            if (sidebarToggle && wrapper) {
+                sidebarToggle.addEventListener('click', function () {
+                    wrapper.classList.toggle('toggled');
+                });
+            }
+        });
+    </script>
+@endsection
